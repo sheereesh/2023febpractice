6258f9d1c96d56dc16918f7fc2e475a155adfdbc
sheereesh <alekhy99@gmail.com>
2023-02-21 19:05:05 +0530

    most of the project completes only hooks and reports remaining

---
+++ src/test/java/testRuns/TestRunner.java
@@ -0,0 +1,27 @@
+package testRuns;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+
+@CucumberOptions(
+		features= {"src/test/resources/features/feature9.feature"}, 
+		
+		glue= {"stepDefinitions","hooks"},
+		plugin= {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+		dryRun = false,
+		monochrome=true
+		
+		
+		
+		
+		)
+
+
+public class TestRunner {
+
+}
